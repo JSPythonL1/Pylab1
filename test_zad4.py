85885c91c1ba41a72a1600b4a85f41dac18a2ace
Dawid <d@ur.pl>
2025-10-06 10:44:14 +0200

    Test

--- test_zad4.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import importlib
-
-@pytest.mark.score(points=2)
-def test_suma_range():
-    zad4 = importlib.import_module("src.zad4")
-    assert hasattr(zad4, "wynik")
-    assert zad4.wynik == sum(range(8, 81))
