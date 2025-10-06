853d5bf2c00a1e68d1925cb72bbe34cc8b45fb81
Dawid Kosior <kosiordawid@gmail.com>
2025-10-06 12:50:24 +0200

    Update test_zad4.py
    fix

--- tests/test_zad4.py
+++ tests/test_zad4.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import sys
 import pytest
 import importlib
@@ -6,7 +7,9 @@ import importlib
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 @pytest.mark.score(points=2)
 def test_suma_range():
-
-    zad4 = importlib.import_module("src.zad4")
-    assert hasattr(zad4, "wynik")
-    assert zad4.wynik == sum(range(8, 81))
+    proc = subprocess.run(
+        ["python", "../src/zad4.py"],
+        text=True,
+        capture_output=True
+    )
+    assert "3212" in proc.stdout
